396b3163cc89d4046d757a7761bbbbe4e897368e
AugustoSus26 <145451648+AugustoSus26@users.noreply.github.com>
2025-05-07 11:04:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -26,5 +26,11 @@ function calculaTempo(tempoObjetivo){
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
-    return horas;
+    let dias = Math.floor(horas/24)
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + "dias" + horas + "horas" + segundos + "segundos";
 }
