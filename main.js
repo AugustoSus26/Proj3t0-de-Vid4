2864ef6ea47878615306a286f47385f98d7b13b2
AugustoSus26 <145451648+AugustoSus26@users.noreply.github.com>
2025-05-07 07:45:52 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,4 +18,13 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-07-09T00:00:00");
 let tempoAtual = new Date()
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    return horas;
+}
